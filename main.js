a8b44a779f8bbc4eb992c736dc12e8ac9002e782
Parthavi29 <72445987+Parthavi29@users.noreply.github.com>
2021-01-12 16:26:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function preload() {
+    world_start = loadSound("world_start.wav");
+    jump = loadSound("jump.wav");
+    mariocoin = loadSound("coin.wav");
+    mariogameover = loadSound("gameover.wav");
+    mariokick = loadSound("kick.wav");
+    mariodie = loadSound("mariodie.wav");
+    setSprites();
+    MarioAnimation();
+}
+
+function setup() {
+    canvas = createCanvas(1240, 336);
+    canvas.parent('canvas');
+
+    instializeInSetup(mario);
+
+    video = createCapture(VIDEO);
+    video.size(600, 300);
+    video.parent('game_console');
+
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on("pose", gotPoses);
+}
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+
+    }
+}
+
+function draw() {
+    game()
+}
